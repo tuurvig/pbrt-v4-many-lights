3c463800cbd8b9adaeff74252a6cf5840fcc6a6a
Richard Kvasnica <rich.kvasnica@gmail.com>
2025-10-28 19:43:30 +0100

    Add sampling from two types of tree in LightcutsLightSampler

--- src/pbrt/lightsamplers/lightcuts.cpp
+++ src/pbrt/lightsamplers/lightcuts.cpp
@@ -9,47 +9,74 @@ namespace pbrt{
 
 STAT_MEMORY_COUNTER("Memory/Lightcuts LightTree", lightCutsLightTreeBytes);
 
+constexpr uint32_t infiniteLightsIndex = 2;
+constexpr uint32_t otherLightsIndex = 3;
+
+LightcutsTree::LightcutsTree(bool isPoint, Allocator alloc) 
+    : lights(alloc), nodes(alloc), isPoint(isPoint) {}
+
 LightcutsLightSampler::LightcutsLightSampler(pstd::span<const Light> lights, Allocator alloc, Float threshold) 
-    : m_lights(alloc), m_infiniteLights(alloc), m_nodes(alloc), m_threshold(threshold) {
+    : m_pointTree(true, alloc), m_spotTree(false, alloc), m_otherLights(alloc), m_lightToLocation(alloc), m_threshold(threshold) {
+    
     // Initialize infiniteLights array and lightcuts lights
-    std::vector<LightBuildContainer> lightcutsLights;
-    SampledWavelengths lambda = SampledWavelengths::SampleVisible(0.5f);
+    std::vector<LightBuildContainer> pointLights, spotLights;
+
     for (size_t i = 0; i < lights.size(); ++i) {
         Light light = lights[i];
         pstd::optional<LightBounds> lightBounds = light.Bounds();
         
         if (!lightBounds) {
+            uint32_t index = m_infiniteLights.size();
             m_infiniteLights.push_back(light);
+            m_lightToLocation.Insert(light, {infiniteLightsIndex, index});
+
         } else if (lightBounds->phi > 0) {
-            // SampledSpectrum phi = SafeDiv(light.Phi(lambda), lambda.PDF());
-            // Float avgPower = phi.Average();
-            lightcutsLights.emplace_back(*lightBounds, light);
-            m_allLightBounds = Union(m_allLightBounds, lightBounds->bounds);
+            if (light.Is<PointLight>()) {
+                pointLights.emplace_back(*lightBounds, light);
+                m_pointTree.allLightBounds = Union(m_pointTree.allLightBounds, lightBounds->bounds);
+
+            } else if (light.Is<SpotLight>()) {
+                spotLights.emplace_back(*lightBounds, light);
+                m_spotTree.allLightBounds = Union(m_spotTree.allLightBounds, lightBounds->bounds);
+
+            } else {
+                uint32_t index = m_otherLights.size();
+                m_otherLights.push_back(light);
+                m_lightToLocation.Insert(light, {otherLightsIndex, index});
+                m_otherLightsPower += lightBounds->phi;
+            }
         }
     }
 
-    if (!lightcutsLights.empty()) {
-        float u = 0.5;
-        buildLightTree(lightcutsLights, 0, lightcutsLights.size(), 0, u);
+    RNG rng;
+    Float u = rng.Uniform<Float>();
+    if (!m_pointTree.lights.empty()) {
+        buildLightTree(pointLights, m_pointTree, 0, pointLights.size(), 0, 0, u);
     }
 
-    lightCutsLightTreeBytes += m_lights.size() * sizeof(Light);
+    if (!m_spotTree.lights.empty()) {
+        buildLightTree(spotLights, m_spotTree, 0, spotLights.size(), 0, 0, u);
+    }
+
+    lightCutsLightTreeBytes += (m_pointTree.lights.size() + m_spotTree.lights.size() + m_otherLights.size() + m_infiniteLights.size()) * sizeof(Light) + 
+                               (m_pointTree.nodes.size() + m_spotTree.nodes.size()) * sizeof(LightcutsTreeNode) +
+                               m_lightToLocation.capacity() * (sizeof(Light) + sizeof(LightLocation));
 }
 
 LightcutsLightSampler::TreeNodeBuildSuccess LightcutsLightSampler::buildLightTree(
-    std::vector<LightBuildContainer>& lightcutsLights, int start, int end, int depth, float& u) {
+    std::vector<LightBuildContainer>& lightcutsLights, LightcutsTree& tree, int start, int end, uint32_t bitTrail, int depth, float& u) {
     DCHECK_LT(start, end);
 
     if (end - start == 1) {
         const LightBuildContainer& leaf(lightcutsLights[start]);
-        CompactLightBounds cb(leaf.bounds, m_allLightBounds);
+        CompactLightBounds cb(leaf.bounds, tree.allLightBounds);
 
-        int nodeIndex = m_nodes.size();
-        int lightIndex = m_lights.size();
+        int nodeIndex = tree.nodes.size();
+        int lightIndex = tree.lights.size();
 
-        m_lights.emplace_back(leaf.light);
-        m_nodes.emplace_back(LightcutsTreeNode::MakeLeaf(lightIndex, nodeIndex, cb));
-        
+        tree.lights.emplace_back(leaf.light);
+        tree.nodes.emplace_back(LightcutsTreeNode::MakeLeaf(lightIndex, nodeIndex, cb));
+        m_lightToLocation.Insert(leaf.light, {tree.isPoint, bitTrail});
         return {leaf.bounds, nodeIndex, nodeIndex};
     }
 
@@ -99,9 +126,10 @@ LightcutsLightSampler::TreeNodeBuildSuccess LightcutsLightSampler::buildLightTre
             rightBoundsPrefix = Union(rightBoundsPrefix, prevRightBoundsPrefix);
         }
 
+        Float diagonalLenSqr = LengthSquared(tree.allLightBounds.Diagonal());
         for (int i = 0, max = nBuckets - 1; i < max; ++i) {
-            const Float leftCost = EvaluateCost(leftBoundsSum[i], true);
-            const Float rightCost = EvaluateCost(rightBoundsSum[i + 1], true);
+            const Float leftCost = EvaluateCost(leftBoundsSum[i], diagonalLenSqr, tree.isPoint);
+            const Float rightCost = EvaluateCost(rightBoundsSum[i + 1], diagonalLenSqr, tree.isPoint);
 
             const Float cost = rightCost + leftCost;
 
@@ -136,42 +164,79 @@ LightcutsLightSampler::TreeNodeBuildSuccess LightcutsLightSampler::buildLightTre
     }
 
     // Allocate interior and recursively initialize children
-    size_t nodeIndex = m_nodes.size();
-    m_nodes.emplace_back();
+    size_t nodeIndex = tree.nodes.size();
+    tree.nodes.emplace_back();
     CHECK_LT(depth, 64);
-    TreeNodeBuildSuccess left = buildLightTree(lightcutsLights, start, mid, depth + 1, u);
+    TreeNodeBuildSuccess left = buildLightTree(lightcutsLights, tree, start, mid, bitTrail, depth + 1, u);
     DCHECK_EQ(nodeIndex + 1, left.nodeIdx);
-    TreeNodeBuildSuccess right = buildLightTree(lightcutsLights, mid, end, depth + 1, u);
+    TreeNodeBuildSuccess right = buildLightTree(lightcutsLights, tree, mid, end, bitTrail | (1u << depth), depth + 1, u);
 
     Float intensities[2] = {left.bounds.phi, right.bounds.phi};
     Float nodePMF;
     int child = SampleDiscrete(intensities, u, &nodePMF, &u);
     int successorIdx = (child == 0) ? left.representantIdx : right.representantIdx;
-    
-    // if (left.bounds.phi == 0)
-    //     successorIdx = right.representantIdx;
-    // else if (right.bounds.phi == 0)
-    //     successorIdx = left.representantIdx;
-    // else {
-    //     Float totalPower = left.bounds.phi + right.bounds.phi;
-    //     Float probLeft = left.bounds.phi / totalPower;
-    //     if (u < probLeft) {
-    //         successorIdx = left.representantIdx;
-    //         u /= probLeft;
-    //     } else {
-    //         successorIdx = right.representantIdx;
-    //         u = (u - probLeft) / (1 - probLeft);
-    //     }
-    // }
 
     LightBounds lb = Union(left.bounds, right.bounds);
-    CompactLightBounds cb(lb, m_allLightBounds);
-    m_nodes[nodeIndex] = LightcutsTreeNode::MakeInterior(right.nodeIdx, successorIdx, cb);
+    CompactLightBounds cb(lb, tree.allLightBounds);
+    tree.nodes[nodeIndex] = LightcutsTreeNode::MakeInterior(right.nodeIdx, successorIdx, cb);
     return {lb, successorIdx, static_cast<int>(nodeIndex)};
 }
 
+pstd::optional<SampledLight> LightcutsLightSampler::SampleLightTree(const LightSampleContext& ctx, const LightcutsTree& tree, Float pmf, Float u) const {
+    int nodeIndex = 0;
+    Point3f p = ctx.p();
+    Normal3f n = ctx.ns;
+    const LightcutsTreeNode* node = &tree.nodes[nodeIndex];
+
+    Float treeDiagonal = LengthSquared(tree.allLightBounds.Diagonal());
+    while (!node->isLeaf) {
+        int childrenIndices[2] = {nodeIndex + 1, node->childOrLightIndex};
+        const LightcutsTreeNode *children[2] = {&tree.nodes[nodeIndex + 1],
+                                                &tree.nodes[node->childOrLightIndex]};
+        
+        Float errBounds[2] = {ComputeErrorBounds(children[0], tree.allLightBounds, p), ComputeErrorBounds(children[1], tree.allLightBounds, p)};
+        if (errBounds[0] == 0 && errBounds[1] == 0) {
+            return {};
+        }
+
+        // Randomly sample a children node
+        Float nodePMF;
+        int child = SampleDiscrete(errBounds, u, &nodePMF, &u);
+        pmf *= nodePMF;
+        nodeIndex = (child == 0) ? (nodeIndex + 1) : node->childOrLightIndex;
+        node = &tree.nodes[nodeIndex];
+
+        if (errBounds[child] < m_threshold) {
+            int representantLightIndex = tree.nodes[node->representantIdx].childOrLightIndex;
+            return SampledLight{tree.lights[representantLightIndex], pmf};
+        }
+    }
+
+    return SampledLight{tree.lights[node->childOrLightIndex], pmf};
+}
+
+pstd::optional<SampledLight> LightcutsLightSampler::SampleInfiniteLight(size_t nLights, Float &pmf, Float &u) const {
+    // Compute infinite light sampling probability _pInfinite_
+    Float pInfinite = Float(m_infiniteLights.size()) /
+                      Float(m_infiniteLights.size() + (nLights == 0 ? 0 : 1));
+
+    if (u < pInfinite) {
+        // Sample infinite lights with uniform probability
+        u /= pInfinite;
+        int index =
+            std::min<int>(u * m_infiniteLights.size(), m_infiniteLights.size() - 1);
+        Float pmf = pInfinite / m_infiniteLights.size();
+        return SampledLight{m_infiniteLights[index], pmf};
+    }
+
+    u = std::min<Float>((u - pInfinite) / (1 - pInfinite), OneMinusEpsilon);
+    pmf = 1 - pInfinite;
+
+    return {};
+}
+
 std::string LightcutsLightSampler::ToString() const {
-    return StringPrintf("[ LightcutsLightSampler nodes: %s ]", m_nodes);
+    return StringPrintf("[ LightcutsLightSampler point tree nodes: %s spot tree nodes: %s ]", m_pointTree.nodes, m_spotTree.nodes);
 }
 
 std::string LightcutsTreeNode::ToString() const {
